4a6626f7f1e96f3828ad63726dc8c8333938838d
FionaGachuuri <fionairuka@gmail.com>
2025-05-28 02:38:41 +0300

    Basic auth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Basic authentication module for API
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """Basic authentication class that inherits from Auth"""
