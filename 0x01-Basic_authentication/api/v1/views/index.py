cf837072c48f6de9e8177ea7221408e43704ef84
FionaGachuuri <fionairuka@gmail.com>
2025-05-27 22:53:52 +0300

    Error handler: Unauthorized

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -6,6 +6,7 @@ from api.v1.views import app_views
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
+
 def status() -> str:
     """ GET /api/v1/status
     Return:
@@ -24,3 +25,11 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
+    """ GET /api/v1/unauthorized
+    Return:
+      - 401 Unauthorized error
+    """
+    abort(401, description="Unauthorized")
