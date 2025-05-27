746153b162ad50e2856e1ea9f2f3cd6b5d0f0d57
FionaGachuuri <fionairuka@gmail.com>
2025-05-28 01:38:21 +0300

    Auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Auth module for managing API authentication
+"""
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """Template for all authentication systems"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Determines if authentication is required for a given path"""
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """Returns the authorization header from the request"""
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Returns the current user"""
+        return None
